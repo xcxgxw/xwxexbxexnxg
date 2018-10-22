bd624474c0029d1e88e527740bceee0175171f38
xcxgxw <44366914+xcxgxw@users.noreply.github.com>
2018-10-22 21:24:05 +0800

    Add files via upload

---
+++ web/mainPage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="design.css">
+    </head>
+
+    <body>
+        <header class="head-container">
+            <nav>
+                <div class="left-nav">
+                    <ul>
+                        <li><a href="#">CCP</li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About Us</a></li>
+                    </ul>
+                </div>
+                <div class="right-nav">
+                    <ul>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <div class="center-container">
+            <img class="image1" src="african.jpg" alt="homeImage">    
+            <button class="button1">Get Started</button>
+        </div>
+    </body>
+</html>
