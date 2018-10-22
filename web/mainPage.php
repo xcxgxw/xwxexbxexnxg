ee255fec3ab9784c9c793531f31e4a3f84e7cd4e
Rick <x3alpine1207@gmail.com>
2018-10-22 23:57:18 +0800

    Home Page Updated

--- web/mainPage.php
+++ web/mainPage.php
@@ -1,31 +1,55 @@
-<!DOCTYPE HTML>
-<html>
-    
-<title> CCP </title>
-<head>
+        <!DOCTYPE HTML>
+        <html>
+            
+        <title>CCP</title>
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">        
         <link rel="stylesheet" type="text/css" href="design.css">
-    </head>
+                <link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
+            </head>
 
-    <body>
-        <header class="head-container">
-            <nav>
-                <div class="left-nav">
-                    <ul>
-                        <li><a href="#">CCP</li>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About Us</a></li>
-                    </ul>
-                </div>
-                <div class="right-nav">
-                    <ul>
-                        <li><a href="#">Login</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        <div class="center-container">
-            <img class="image1" src="african.jpg" alt="homeImage">    
-            <button class="button1">Get Started</button>
+            <body>
+            
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Company Collaboration Platform</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            </ul>
+            
+            <form action ="loginPage.php">
+                    <input type ="submit" class ="btn btn-secondary btn-sm my-2 my-sm-0" name = "logInBtn" value = "Login ">
+            </form>  
+
         </div>
-    </body>
-</html>
+        </nav>    
+
+
+
+            <!-- <div class="jumbotron text-center">
+            <img class="image1" src="african.jpg" alt="homeImage"> 
+                <h1>VizHub ™ : Data Insight Provider</h1>
+            </div> -->
+          
+
+
+            <div class="center-container">
+                <img src="african.jpg" class="img-fluid">
+                <button type="button" class="btn btn-dark my-2 my-sm-0">Get Started</button>
+            </div>
+        </div>
+    
+        </body>
+
+    </html>
