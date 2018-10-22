21cd565f6a3e7cc20e0312640f6f15606d62ac58
chestnutbunker <kelvin_foo_13vin@hotmail.com>
2018-10-23 00:31:27 +0800

    Logo and Href added.

--- web/aboutus.php
+++ web/aboutus.php
@@ -1,29 +1,47 @@
 <!DOCTYPE HTML>
-<html>
-    
-<title> CCP </title>
-<head>
+        <html>
+            
+        <title>CCP</title>
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">        
         <link rel="stylesheet" type="text/css" href="design.css">
-    </head>
+                <link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
+            </head>
 
-    <body>
-        <header class="head-container">
-            <nav>
-                <div class="left-nav">
-                    <ul>
-                        <li><img class="image2" src="team.png" alt="ccplogo" width="40" height="30"></li>
-                        <li><a href="#">Company Collaborative Platform</li>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="http://localhost/xwxexbxexnxg/web/aboutus.php">About Us</a></li>
-                    </ul>
-                </div>
-                <div class="right-nav">
-                    <ul>
-                        <li><a href="#">Login</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        
-    </body>
-</html>
+            <body>
+            
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <img class="logo" src="logo.png" alt="ccplogo" width="40" height="30">
+        <a class="navbar-brand" href="#">Company Collaboration Platform</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/xwxexbxexnxg/web/mainPage.php">Home</a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/xwxexbxexnxg/web/aboutus.php">About Us</a>
+            </li>
+            </ul>
+            
+            <form action ="loginPage.php">
+                    <input type ="submit" class ="btn btn-secondary btn-sm my-2 my-sm-0" name = "logInBtn" value = "Login ">
+            </form>  
+
+        </div>
+        </nav>    
+
+            <div class="center-container">
+                <img src="african.jpg" class="img-fluid">
+                <button type="button" class="btn btn-dark my-2 my-sm-0">Get Started</button>
+            </div>
+        </div>
+    
+        </body>
+
+    </html>
