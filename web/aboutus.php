51580a4c849e8d872e0b545164c18418c59f11bc
chestnutbunker <kelvin_foo_13vin@hotmail.com>
2018-10-23 00:29:43 +0800

    Logo added. Href added.

---
+++ web/aboutus.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+    
+<title> CCP </title>
+<head>
+        <link rel="stylesheet" type="text/css" href="design.css">
+    </head>
+
+    <body>
+        <header class="head-container">
+            <nav>
+                <div class="left-nav">
+                    <ul>
+                        <li><img class="image2" src="team.png" alt="ccplogo" width="40" height="30"></li>
+                        <li><a href="#">Company Collaborative Platform</li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="http://localhost/xwxexbxexnxg/web/aboutus.php">About Us</a></li>
+                    </ul>
+                </div>
+                <div class="right-nav">
+                    <ul>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        
+    </body>
+</html>
