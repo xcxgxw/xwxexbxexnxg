7c043d33776a4bc387b23ead7b6c1b3b1d166169
Rick <x3alpine1207@gmail.com>
2018-10-23 22:26:23 +0800

    asd

--- web/discussion_thread.php
+++ web/discussion_thread.php
@@ -1,37 +1,97 @@
 <!DOCTYPE HTML>
         <html>
+
+
+     <script>
+     
+     
+     function loadHR(){
+
+  document.getElementById("dept_name").innerHTML = "Human Resource Department"; 
+  document.getElementById("ch_no.").innerHTML ="(5)";
+  document.getElementById("li1").style.visibility = "visible";
+      document.getElementById("li2").style.visibility = "visible";
+      document.getElementById("li3").style.visibility = "visible";
+
+      var sp1 = document.createElement("span");
+        // give it an id attribute called 'newSpan'
+        sp1.id = "span1";
+        // create some content for the new element.
+        var sp1_content = document.createTextNode("Expensive Canteen Food");
+        // apply that content to the new element
+        sp1.appendChild(sp1_content);
+        // build a reference to the existing node to be replaced
+        var sp2 = document.getElementById("span1");
+        var parentDiv = sp2.parentNode;
+        // replace existing node sp2 with the new span element sp1
+        parentDiv.replaceChild(sp1, sp2);
+
+        var sp3 = document.createElement("span");
+        sp3.id = "span2";
+        var sp3_content = document.createTextNode("Low Salary");
+        sp3.appendChild(sp3_content);
+        var sp4 = document.getElementById("span2");
+        var parentDiv = sp4.parentNode;
+        parentDiv.replaceChild(sp3, sp4);
+
+
+
+
+}
+     
+     function loadMG(){
+
+       document.getElementById("dept_name").innerHTML = "Management";
+       document.getElementById("ch_no.").innerHTML ="(2)";
+      
+        var sp1 = document.createElement("span");
+        sp1.id = "span1";
+        var sp1_content = document.createTextNode("Leadership");
+        sp1.appendChild(sp1_content);
+        var sp2 = document.getElementById("span1");
+        var parentDiv = sp2.parentNode;
+        parentDiv.replaceChild(sp1, sp2);
+
+        var sp3 = document.createElement("span");
+        sp3.id = "span2";
+        var sp3_content = document.createTextNode("Managing People");
+        sp3.appendChild(sp3_content);
+        var sp4 = document.getElementById("span2");
+        var parentDiv = sp4.parentNode;
+        parentDiv.replaceChild(sp3, sp4);
+
+      document.getElementById("li1").style.visibility = "hidden";
+      document.getElementById("li2").style.visibility = "hidden";
+      document.getElementById("li3").style.visibility = "hidden";
+      
+    }
+
+     
+     </script>   
+     
      <link rel ="stylesheet" href = "threadDesign.css" type = "text/css">
-
-      <style>ul.channels_lists{list-style-type: none;}</style>
-
-
-
 
             <body>
         <div class="slack">
   <nav class="teams">
     <ul class="teams__list">
       <li class="teams__item">
-        <button class="teams__button teams__button--active">
+        <button class="teams__button teams__button--active" onclick="loadHR()">
           HR
         </button>
       </li>
       <li class="teams__item">
-        <button class="teams__button">
+        <button class="teams__button" onclick="loadMG()">
           MG
         </button>
       </li>
-      <li class="teams__item">
-        <button class="teams__button">
-          PS
-        </button>
-      </li>
+     
     </ul>
   </nav>
   <div class="sidebar">
     <button class="team-menu">
       <div class="team-menu__info">
-        <h1 class="team-menu__name">Human Resource Department</h1>
+        <h1 class="team-menu__name "id ="dept_name" >Human Resource Department</h1>
         <div class="team-menu__status">
           <span class="team-menu__username">burgerhong</span>
         </div>
@@ -41,30 +101,36 @@
     <div class="threads">
       <span class="ion-chatbubble-working threads__icon"></span> All Threads
     </div>
-    <script
+
     <div class="channels">
       <h2 class="channels__heading">
-        <span>Lists <span class="channels__number">(5)</span>
+        <span>Topics <span class="channels__number" id ="ch_no."></span>
         </span>
         <button class="ion-ios-plus-outline channels__add"></button>
       </h2>
-      <ul class="channels__list">
+      <ul class="channels__list" id="HR_topic">
         <li class="channels__item">
-          <button onclick="comment1()" class="channels__button"><span>Expensive Canteen Food</span></button>
+          <button onclick="comment1()" class="channels__button"><span id= "span1">Expensive Canteen Food</span></button>
         </li>
         <li class="channels__item">
-          <button onclick="comment2()" class="channels__button"><span>Low Salary</span></button>
+          <button onclick="comment2()" class="channels__button"><span id = "span2">Low Salary</span></button>
         </li>
-        <li class="channels__item">
-          <button class="channels__button"><span>Company Annual Trip</span></button>
+        <li class="channels__item" id="li1">
+          <button class="channels__button"><span id ="span" >Company Annual Trip</span></button>
         </li>
-        <li class="channels__item">
-          <button class="channels__button"><span>Bonding Camp</span></button>
+        <li class="channels__item" id="li2">
+          <button class="channels__button"><span  id ="span">Bonding Camp</span></button>
         </li>
-        <li class="channels__item">
-          <button class="channels__button"><span>Annual Dinner</span></button>
+        <li class="channels__item" id="li3">
+          <button class="channels__button"><span id ="span" >Annual Dinner</span></button>
         </li>
       </ul>
+
+
+
+
+
+
       <form action="calendar.php">
       <button class="team-menu__name" style ="padding-left: 15px ;padding-top: 10px">
             Calendar
