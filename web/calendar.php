4f771035b19d2e2668fc0e7030244ead2c7ec4f8
senglol <senglol0710@gmail.com>
2018-10-23 14:58:38 +0800

    updated

--- web/calendar.php
+++ web/calendar.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Event Calendar</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="calendar2.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="calendar.css" />
     <script src="main.js"></script>
 </head>
 <body>
