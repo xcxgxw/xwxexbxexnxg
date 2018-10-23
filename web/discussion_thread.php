584578c8a979a3cca38ecda31cb5148f0a030033
burgerhong <jason_tan1996@hotmail.com>
2018-10-23 19:39:37 +0800

    hkjhkh

--- web/discussion_thread.php
+++ web/discussion_thread.php
@@ -41,7 +41,6 @@
     <div class="threads">
       <span class="ion-chatbubble-working threads__icon"></span> All Threads
     </div>
-    <script
     <div class="channels">
       <h2 class="channels__heading">
         <span>Lists <span class="channels__number">(5)</span>
@@ -76,19 +75,19 @@
     
   </div>
   <div class='app-layout'>
-  <div class='header box'>TITLE</div>
+  <div class='header box' ><p id="title"></p></div>
     <div class='messages box'>
       <ul class='message-list'>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li><p id="thread1"></p> </li>
+        <li><p id="thread2"></p></li>
+        <li><p id="thread3"></p></li>
+        <li><p id="thread4"></p></li>
+        <li><p id="thread5"></p></li>
+        <li><p id="thread6"></p></li>
+        <li><p id="thread7"></p></li>
+        <li><p id="thread8"></p></li>
+        <li><p id="thread9"></p></li>
+        <li><p id="thread10"></p></li>
         <li></li>
         <li></li>
         <li></li>
@@ -100,6 +99,24 @@
     </div>
 </div>
 </div>
+
+    <script>
+        function comment1(){
+            document.getElementById("title").innerHTML = "Expensive Canteen Food";
+            document.getElementById("thread1").innerHTML ="burgerhong: Yesterday I ate kuay png, it costed my RM 16.90, damn expensive sia";
+            document.getElementById("thread2").innerHTML ="senglol: Ya lo, walao how eat wor like this";
+            document.getElementById("thread3").innerHTML ="burgerhong: Let's report it to boss ok?";
+            document.getElementById("thread4").innerHTML ="senglol: Okok on";
+            }
+
+        function comment2(){
+            document.getElementById("title").innerHTML = "Low Salary";
+            document.getElementById("thread1").innerHTML ="A: Recently i notice after SST, my income is not enough for my outcome";
+            document.getElementById("thread2").innerHTML ="B: ya lor";
+            document.getElementById("thread3").innerHTML ="C: I think we need to propose this to our boss Mr Beh";
+            document.getElementById("thread4").innerHTML ="";
+        }
+     </script>
         
     </body>
 
