094e9fc8ab9f0583834644795fcf4466d8e27667
Rick <x3alpine1207@gmail.com>
2018-10-23 15:04:54 +0800

    JS script source

--- web/aboutus.php
+++ web/aboutus.php
@@ -7,6 +7,9 @@
             <meta name="viewport" content="width=device-width, initial-scale=1">        
             <link rel="stylesheet" type="text/css" href="design.css">
             <link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" ></script>
         </head>
 
         <body>
--- web/mainPage.php
+++ web/mainPage.php
@@ -7,15 +7,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">     
         <link rel="stylesheet" type="text/css" href="design.css">
                 <link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" ></script>
+            
             </head>
 
             <body>
             
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <img class="logo" src="logo.png" alt="ccplogo" width="40" height="30">
+        
         <a class="navbar-brand" href="#">Company Collaboration Platform</a>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
+        
         </button>
 
         <div class="collapse navbar-collapse" id="navbarColor01">
@@ -34,6 +41,10 @@
 
         </div>
         </nav>    
+
+
+
+        
 
             <div class="center-container">
                 <img src="collab.jpg" class="img-fluid">
