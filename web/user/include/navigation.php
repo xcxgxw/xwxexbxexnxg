0e67efedde131d0d302c3f668cf9177bb4146c29
gheewei1608 <43561828+gheewei1608@users.noreply.github.com>
2018-12-16 01:52:52 +0800

    profile

--- web/user/include/navigation.php
+++ web/user/include/navigation.php
@@ -43,7 +43,7 @@
                         });
                     } else {
                         // User is not signed in.
-                        alert("Sign In Failed");
+                        console.log("Sign In Failed");
                     }
                 });
                 }
@@ -58,13 +58,13 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i id="getUserName" class="fa fa-user"></i><b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
-                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                            <a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a>
                         </li>
                         <li class="divider"></li>
                         <li>
                             <!-- <a href="../loginPage.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a> -->
                             <!-- <button class="fa fa-fw fa-power-off" onclick="logout();">Log Out</button> -->
-                            <button class="btn btn-danger" onclick="logout();">Log Out</button>
+                            <button class="btn btn-danger" style="margin-left:20px;" onclick="logout();">Log Out</button>
                         </li>
                     </ul>
                 </li>
