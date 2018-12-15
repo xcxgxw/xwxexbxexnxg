12a5c10f1f3ca5b5efb1b54d6fd570a254686b32
Rick <x3alpine1207@gmail.com>
2018-12-15 13:47:24 +0800

    discussion update

--- web/user/discussion_thread.php
+++ web/user/discussion_thread.php
@@ -5,6 +5,7 @@
 <script src="https://www.gstatic.com/firebasejs/5.7.0/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/5.7.0/firebase-database.js"></script>
 <script src="https://www.gstatic.com/firebasejs/5.7.0/firebase-auth.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script>
   // Initialize Firebase
   var config = {
@@ -33,118 +34,116 @@ writeUserData("Ghee Wei","gw@gmail.com","123");
 </script> -->
 
 <script>
-  function comment1(){
-    document.getElementById("title").innerHTML = "Expensive Canteen Food";
-  }
+  
+function disp(){  
 
-  function comment2(){
-    document.getElementById("title").innerHTML = "Low Salary";
-  }
-           
-  function addtopic(){
-    var topic = prompt("Please enter your topic title");
-    if (topic != null) {
-      document.getElementById("span3").innerHTML = topic
-      document.getElementById("title").innerHTML = topic
-      document.getElementById("thread1").innerHTML ="";
-      document.getElementById("thread2").innerHTML ="";
-      document.getElementById("thread3").innerHTML ="";
-      document.getElementById("thread4").innerHTML ="";
-    }
-    document.getElementById("li1").style.visibility = "visible";
-    document.getElementById("ch_no.").innerHTML ="(3)";
-  }
+  var message_list = $(".message-list");
+  var userDataRef = firebase.database().ref("forum").orderByKey();
+  userDataRef.once("value").then(function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+    // var key = childSnapshot.key;
+    // var childData = childSnapshot.val();              
+    var message = childSnapshot.val().message;
+    var time = childSnapshot.val().postedTime;
+    var listItem = document.createElement("li");
+    listItem.textContent = message;
+    message_list.append(listItem);
+    console.log(message_list);
+    });
+  });
+}
+
+function iconn(int_par){
+  
+  var iconn_list = $(".teams__list");
+  var userDataRef = firebase.database().ref("departmentHierarchy").child(int_par).orderByKey();
+  userDataRef.once("value").then(function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+    var key = childSnapshot.key;
+
+    var listItem = document.createElement("li");
+    listItem.setAttribute('class','teams_item');
+    var item = document.createElement("button");
+    item.setAttribute('class','teams__button');
+    item.setAttribute('id', key);
+    item.setAttribute('onclick','getTextValue(this.id)');
+    item.textContent = key;
+    listItem.appendChild(item);
+    
+    iconn_list.append(listItem);   
+    
+    });
+  });
+
+}
+
+function getTextValue(objButton){
+
+  var text = objButton;
+  dispTopic(text);
+
+}
+
+function dispTopic(button_key){
+
+  $(".team-menu__info").empty();
+  var userDataRef = firebase.database().ref("departmentList").child(button_key).orderByKey();
+  userDataRef.once("value").then(function(snapshot) {
+  // snapshot.forEach(function(childSnapshot) {
+    var departName = snapshot.val().name;
+    var div = $(".team-menu__info");
+   header = document.createElement("h1");
+   header.setAttribute('class','team-menu__name');
+   header.textContent = departName;
+   div.append(header);
      
-  function loadHR(){
-    document.getElementById("dept_name").innerHTML = "Human Resource Department"; 
-    document.getElementById("ch_no.").innerHTML ="(5)";
-    document.getElementById("li1").style.visibility = "visible";
-    document.getElementById("li2").style.visibility = "visible";
-    document.getElementById("li3").style.visibility = "visible";
+    // });
+  });
+  
+  
+  var topic_list = $(".channels__list");
+  $(".channels__list").empty();
+var userDataRef = firebase.database().ref("departmentList").child(button_key).child("topicList").orderByKey();
+  userDataRef.once("value").then(function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+    var topic = childSnapshot.key;
+    listItem = document.createElement("li");
+    listItem.setAttribute('class','channels__item');
+    var item = document.createElement("button");
+    item.setAttribute('class','channels__button');
+    item.setAttribute('id', topic);
+    item.setAttribute('onclick','getDepartmentValue(this.id)');
+    item.textContent = topic;
+    listItem.appendChild(item);
+    topic_list.append(listItem);  
+    });
+  });
 
-    var sp1 = document.createElement("span");
-    // give it an id attribute called 'newSpan'
-    sp1.id = "span1";
-    // create some content for the new element.
-    var sp1_content = document.createTextNode("Expensive Canteen Food");
-    // apply that content to the new element
-    sp1.appendChild(sp1_content);
-    // build a reference to the existing node to be replaced
-    var sp2 = document.getElementById("span1");
-    var parentDiv = sp2.parentNode;
-    // replace existing node sp2 with the new span element sp1
-    parentDiv.replaceChild(sp1, sp2);
-
-    var sp3 = document.createElement("span");
-    sp3.id = "span2";
-    var sp3_content = document.createTextNode("Low Salary");
-    sp3.appendChild(sp3_content);
-    var sp4 = document.getElementById("span2");
-    var parentDiv = sp4.parentNode;
-    parentDiv.replaceChild(sp3, sp4);
-  }
+}
      
-     function loadMG(){
-
-       document.getElementById("dept_name").innerHTML = "Management";
-       document.getElementById("ch_no.").innerHTML ="(2)";
-      
-        var sp1 = document.createElement("span");
-        sp1.id = "span1";
-        var sp1_content = document.createTextNode("Leadership");
-        sp1.appendChild(sp1_content);
-        var sp2 = document.getElementById("span1");
-        var parentDiv = sp2.parentNode;
-        parentDiv.replaceChild(sp1, sp2);
-
-        var sp3 = document.createElement("span");
-        sp3.id = "span2";
-        var sp3_content = document.createTextNode("Managing People");
-        sp3.appendChild(sp3_content);
-        var sp4 = document.getElementById("span2");
-        var parentDiv = sp4.parentNode;
-        parentDiv.replaceChild(sp3, sp4);
-
-      document.getElementById("li1").style.visibility = "hidden";
-      document.getElementById("li2").style.visibility = "hidden";
-      document.getElementById("li3").style.visibility = "hidden";
-      
-    }
-
 </script>   
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel ="stylesheet" href = "threadDesign.css" type = "text/css">
 
-<body background ="https://i.imgur.com/QVFUaZP.jpg?1">
+<body background ="https://i.imgur.com/QVFUaZP.jpg?1" onload = "iconn(3)">
 <div class="slack">
   <nav class="teams">
     <ul class="teams__list">
-      <li class="teams__item">
+      <!-- <li class="teams__item">
         <button class="teams__button" onclick="loadHR()">
           HR
         </button>
-      </li>
-      <li class="teams__item">
-        <button class="teams__button" onclick="loadMG()">
-          MG
-        </button>
-      </li>
+      </li> -->
     </ul>
   </nav>
 <div class="sidebar">
   <button class="team-menu">
     <div class="team-menu__info">
-      <h1 class="team-menu__name "id ="dept_name" >Human Resource Department</h1>
-      <div class="team-menu__status">
-        <span class="team-menu__username">Ivory</span>
-      </div>
+      <!-- <h1 class="team-menu__name"></h1> -->
     </div>
     <span class="team-menu__alarm ion-ios-bell-outline"></span>
   </button>
-  <div class="threads">
-    <span class="ion-chatbubble-working threads__icon"></span> All Threads
-  </div>
 
   <div class="channels">
     <h2 class="channels__heading">
@@ -152,33 +151,17 @@ writeUserData("Ghee Wei","gw@gmail.com","123");
       </span>
       <button class="ion-ios-plus-outline channels__add"></button>
     </h2>
-      <ul class="channels__list" id="HR_topic">
-        <li class="channels__item">
-          <button onclick="comment1()" class="channels__button"><span id= "span1">Expensive Canteen Food</span></button>
-        </li>
-        <li class="channels__item">
-          <button onclick="comment2()" class="channels__button"><span id = "span2">Low Salary</span></button>
-        </li>
-        <li class="channels__item" id="li1">
-          <button class="channels__button"><span id ="span3" >Company Annual Trip</span></button>
-        </li>
-        <li class="channels__item" id="li2">
-          <button class="channels__button"><span  id ="span">Bonding Camp</span></button>
-        </li>
-        <li class="channels__item" id="li3">
-          <button class="channels__button"><span id ="span" >Annual Dinner</span></button>
-        </li>
-        <li>
-        <button onclick="addtopic()" class="team-menu" ><h1 class="team-menu__name">Start A New Topic</h1>
-          </button>
-          </li>
-          <li>
-        <form action="calendar.php">
-      <button class="team-menu"><h1 class="team-menu__name">Company Event Calendar</h1>
-          </button>
-          </form>
-          </li> 
+      <ul class="channels__list">
       </ul>
+
+      <div class="channels">       
+              <button onclick="onload()" class="team-menu" ><h1 class="team-menu__name">Start A New Topic</h1>
+                </button>              
+              <form action="calendar.php">
+            <button class="team-menu"><h1 class="team-menu__name">Company Event Calendar</h1>
+                </button>
+                </form>
+      </div>
     </div>
 
 
@@ -188,16 +171,6 @@ writeUserData("Ghee Wei","gw@gmail.com","123");
   <div class='header box'><p id="title"></p></div>
     <div class='messages box'>
       <ul class='message-list'>
-        <li><p id="thread1"></p> </li>
-        <li><p id="thread2"></p></li>
-        <li><p id="thread3"></p></li>
-        <li><p id="thread4"></p></li>
-        <li><p id="thread5"></p></li>
-        <li><p id="thread6"></p></li>
-        <li><p id="thread7"></p></li>
-        <li><p id="thread8"></p></li>
-        <li><p id="thread9"></p></li>
-        <li><p id="thread10"></p></li>
       </ul>
     </div>
     <div class='input box'>
@@ -210,12 +183,21 @@ writeUserData("Ghee Wei","gw@gmail.com","123");
         var message = document.getElementById("inputMessage").value;
   
         firebase.database().ref().child('forum').push({
-          uid: userId,
           message: message,
           postedTime: currentTime,
         });
       }
-      document.getElementById("submit").addEventListener("click",writeUserData);
+      
+      $("#submit").click(function(){
+        $(".message-list").empty();
+        writeUserData();
+        disp();
+});
+
+      // $(".teams__button").click(function(){
+      //         var text = $(".teams_button").textContent;
+      //         console.log("123");
+      // });
       </script>
     </div>
 </div>
