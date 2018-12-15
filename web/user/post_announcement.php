1da12a0c28a2328029fdcea86ef78d880d7d98b1
senglol <senglol0710@gmail.com>
2018-12-15 23:46:37 +0800

    can post announcement

--- web/user/post_announcement.php
+++ web/user/post_announcement.php
@@ -2,6 +2,25 @@
 <?php include "include/footer.php"?>
 <?php include "include/navigation.php"?>
 
+<script src="https://www.gstatic.com/firebasejs/5.7.0/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.7.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.7.0/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.7.0/firebase-auth.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyARKMgF5Ik_20WkcChSNYrKh_Sxt17CG00",
+    authDomain: "webengrg.firebaseapp.com",
+    databaseURL: "https://webengrg.firebaseio.com",
+    projectId: "webengrg",
+    storageBucket: "webengrg.appspot.com",
+    messagingSenderId: "708972282948"
+  };
+  firebase.initializeApp(config);
+</script>
+
 <div id="wrapper">
 
 
@@ -24,27 +43,27 @@
              echo $_POST['title'];
          }
          ?>
-      <form action="" method="post" enctype="multipart/form-data">
+      <!-- <form action="post_announcement.php" method="post" enctype="multipart/form-data"> -->
          <div class ="form-group">
             <label for="title">Anncouncement Title</Label>
-            <input type="text" class="form-control" name="title">
+            <input type="text" class="form-control" id="title">
          </div>
 
          <div class="form-group">
             <label for="post_category">Post Category Id</label>
-            <input type="text" class="form-control" name="post_category_id">
+            <input type="text" class="form-control" id="post_category_id">
          </div>
 
          <div class ="form-group">
             <label for="post_content">Post Content</Label>
-            <textarea class="form-control" name="post_content" id="" cols="30" rows="10">
-            </textarea>
+            <textarea class="form-control" id="post_content" cols="30" rows="10"></textarea>
          </div>
          
          <div class="form-group">
-            <input class="btn btn-primary" type="submit" name="create_post" value="Publish Announcement">
+            <input class="btn btn-primary" type="submit" id="submit" value="Publish Announcement">
+
          </div>
-      </form>
+      <!-- </form> -->
       
                 </div>
             </div>
@@ -54,3 +73,24 @@
         <!-- /.container-fluid -->
 
         </div>
+   <script>
+  function announcement() {
+    // var userId = firebase.auth().currentUser.uid;
+    var currentTime = new Date().toLocaleString();
+    var title = document.getElementById("title").value;
+    var content = document.getElementById("post_content").value;
+
+    firebase.database().ref().child("announcement").push({
+      title: title,
+      content: content,
+      postedTime: currentTime
+    });
+    document.getElementById("title").value="";
+    document.getElementById("post_content").value="";
+  }
+  $("#submit").click(function(){
+        announcement();
+        alert("Announcement published successfully!")
+});
+
+  </script>
